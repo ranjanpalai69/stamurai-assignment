e25cb5af823cd890e194bce508e417328a0fa9cb
RANJAN PALAI <palairanjan2002@gmail.com>
2023-07-12 21:44:08 +0530

    added next + ts template

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Task Management App',
+  description: 'A Simple Task Management Application Made with Next.Js And TypeScript',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
